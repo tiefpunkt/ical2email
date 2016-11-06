07323ca5a5c6ff8be65a2d541ca6be1293b516cb
Severin Schols <severin@schols.de>
2016-11-07 00:35:50 +0100

    updated events in recurrances were sent out twice (old+new)

--- ical2email.py
+++ ical2email.py
@@ -38,7 +38,7 @@ def send_mail(event, dtstart):
 	server.login(config.SMTP_USER, config.SMTP_PASS)
 	text = msg.as_string()
 	server.sendmail(config.MAIL_FROM, config.MAIL_TO, text)
-	server.quit()	
+	server.quit()
 
 tz = timezone(config.ICAL_TZ)
 
@@ -51,6 +51,8 @@ today = datetime.now(tz).replace(hour=0,minute=0)
 time_min = today + timedelta(days=config.DAYS_AHEAD)
 time_max = today + timedelta(days=config.DAYS_AHEAD + 1)
 
+events = []
+
 for event in cal.walk('vevent'):
 	dtstart = event.get('dtstart').dt
 
@@ -60,10 +62,35 @@ for event in cal.walk('vevent'):
 		dtstart = dtstart.astimezone(tz)
 
 	if dtstart > time_min and dtstart < time_max:
-		send_mail(event, dtstart)
+		#send_mail(event, dtstart)
+		events.append((event, dtstart))
 	else:
 		# handle reoccurrences
 		if "rrule" in event:
 			rule = rrule.rrulestr(event.get('rrule').to_ical(), dtstart=event.get('dtstart').dt)
 			for dtstart_rec in rule.between(time_min, time_max):
-				send_mail(event, dtstart_rec)
+				#send_mail(event, dtstart_rec)
+				events.append((event, dtstart_rec))
+
+def check_for_modified_reoccurences(old_items, new_item):
+	# check all events for duplicate UIDs
+	for n, item in enumerate(old_items):
+		if item[0].get('uid') == new_item[0].get('uid'):
+			# Duplicate UID found
+			# The original item has no "RECURRENCE-ID" set, the updated one does
+			if "RECURRENCE-ID" in new_item[0]:
+				# new item the updated one
+				# remove the old entry, fill in the new one
+				del old_items[n]
+				old_items.append(new_item)
+			# if the old one is the updated one, just ignore the new item and move on
+			return old_items
+
+	# first item, or simply duplicate UIDs found
+	old_items.append(new_item)
+	return old_items
+
+events = reduce(check_for_modified_reoccurences, events, [])
+
+for event in events:
+	send_mail(event[0], event[1])
